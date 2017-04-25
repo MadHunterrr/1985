5095eb75280bb6c88c32931f50ab657f1500d968
MadHunter <evgeniy-bylym@rambler.ru>
2017-04-25 22:23:26 +0300

    daytime v1.0

---
+++ Assets/Scripts/Editor/EnvironmentEditorScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class EnvironmentEditorScript : EditorWindow
+{
+
+    [MenuItem("Window/Environment")]
+
+    public static void ShowWindow()
+    {
+        GetWindow(typeof(EnvironmentEditorScript));
+    }
+
+    void OnGUI()
+    {
+        try
+        {
+            Environment en = FindObjectOfType<Environment>();
+            EditorGUILayout.LabelField("Время");
+            en.timeHour = (byte)EditorGUILayout.IntSlider(new GUIContent("Часов: ", "Время игры в игровых часах"), en.timeHour, 0, 23);
+            en.timeMinute = (byte)EditorGUILayout.IntSlider(new GUIContent("Минут: ", "Время игры в игровых часах(минутах)"), en.timeMinute, 0, 59);
+            en.TimeMultiple = EditorGUILayout.Slider(new GUIContent("Множитель времени: ", "Если установить значение в 1 - то 1 игровая минута будет равняться 1 секунде реального времени"), en.TimeMultiple, 1, 60);
+            en.SunAngle();
+        }
+        catch
+        {
+            Debug.Log("Нет менеджера окружения");
+        }
+    }
+
+}
---
+++ Assets/Scripts/Environment.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Environment : MonoBehaviour
+{
+    [Range(0, 23)]
+    public byte timeHour;
+    [Range(0, 23)]
+    public byte timeMinute;
+    public const float AnglePerMinute = 0.25f;
+    public const float AnglePerHour = 15f;
+    [Range(1, 60)]
+    public float TimeMultiple = 1; //(60-realtime)(1 game minyte = 1 real secounds
+    public int DayCount = 0;
+
+
+
+    public Light Sun
+    {
+        get
+        {
+            return RenderSettings.sun;
+        }
+    }
+
+
+    public void SunAngle()
+    {
+
+        float angle = (timeHour * AnglePerHour) + (timeMinute * AnglePerMinute);
+        Sun.transform.rotation = Quaternion.Euler(angle - 90, -90, 30);
+    }
+
+
+    public float lastCheckTime = 0;
+    void ChangeTime()
+    {
+        if (timeMinute < 59)
+            timeMinute++;
+        else
+        {
+            if (timeHour < 23)
+                timeHour++;
+            else
+                timeHour = 0;
+            timeMinute = 0;
+        }
+        SunAngle();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= lastCheckTime + TimeMultiple)
+        {
+            ChangeTime();
+            lastCheckTime = Time.time;
+        }
+    }
+
+
+}
